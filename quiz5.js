0ae9c1fa903c67063c1631ba72a31ab31320a116
Mickell Ford II <31451009+onyxprime0@users.noreply.github.com>
2018-02-27 12:51:21 -0500

    Add files via upload

---
+++ quiz5.js
@@ -0,0 +1,12 @@
+function setup() {
+  createCanvas(500,500);
+}
+
+function draw() {
+var c1 = color(255, 255, 0);
+ var c2 = color(255, 0, 0);
+ var c3 = color(0, 0, 255);
+background(244, 248, 252);
+line(mouseX, 0, mouseX, 100);
+noStroke(); 
+}
